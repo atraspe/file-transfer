1fff051875b95bf83a7eac9dc7019cf719d15579
Art <atraspe@yahoo.com>
2019-04-30 08:58:11 -0400

    Added parsing the csv files, and more update to logic

--- fts.py
+++ fts.py
@@ -2,45 +2,56 @@ import sys
 from pathlib import Path
 import argparse
 
-# SSH
-import base64
-import paramiko
+# CSV
+import csv
 
 # FTP
 import ftplib
 
 
-prog_desc = 'purpose: transfer file to or from a host that is behind a UNIX gateway.'
+
+prog_desc = 'purpose: transfer file to or from a host that is behind a UNIX gateway. unless specified (as binary), file will be treated as ASCII.'
 line = '============================================================================'
-gateway_desc = 'UNIX gateway to be used. Currently supported gateways are:\ngate-sn.avctr.gxs.com (Amstelveen)\n and gate-sn.ohctr.gxs.com (Ohio).'
 
 parser = argparse.ArgumentParser(description=prog_desc, add_help=False)
 parser.add_argument('file', help='file to be transferred')
+parser.add_argument('-b', '--binary', help='if not specified, file will be treated as ASCII', action='store_true')
 parser.add_argument('-g', '--gateway', help='UNIX gateway to be used')
 parser.add_argument('-u', '--username', help='Gateway username')
-parser.add_argument('-t', '--token', help='RSA SecurID token ID')
-parser.add_argument('-i', '--instance', help='instance')
+parser.add_argument('-p', '--passcode', help='MobilePASS OHGate - IDLDAP.NET passcode')
+parser.add_argument('-i', '--instance', help='MS client instance')
 parser.add_argument('-a', '--action', help='download or upload')
 parser.add_argument('-v', '--verbose', help='explain what is being done', action='store_true')
-parser.add_argument('-h', '--help', help='show this help message and exit', action='help')#, default=argparse.SUPPRESS)
+parser.add_argument('-h', '--help', help='show this help message and exit', action='help')
 
 args = parser.parse_args()
 
-gateway = {1 : 'gate-sn.ohctr.gxs.com', 2 : 'gate-sn.avctr.gxs.com'}
-instance = {1 : 'baldis1', 2 : 'baldis2'}
-action = {1 : 'download', 2 : 'upload'}
 
-# sample dictionary for client accounts {'instance' : {'hostname' : 'x', 'ftp password' : 'abc', 'clientid' : 'XYZ'}}
-client_accounts = {'buaour1' : {'hostname' : 'lit-vams-b102.gxsonline.net', 'password' : '7Ey4s|Ms', 'clientid' : 'UAOUR'}}
-
-def ask(question, dict_val):
+def prompt(header, ask, response_type='str', main_dict=None, temp_dict=None):
     while True:
         try:
-            print(line)
-            answer = int(input(question))
-            choice = dict_val[answer]
-        except (ValueError, KeyError):
+            print(f'{line}\n{header}')
+            if main_dict or temp_dict:
+                d = temp_dict if temp_dict else main_dict
+
+                for k,v in d.items():
+                    print(f'{k} : {v}')
+
+            answer = input(f'{ask}')
+            choice = answer
+
+            if response_type == 'int':
+                answer = int(answer)
+
+            if temp_dict:
+                choice_tmp = temp_dict[answer]
+                answer = choice_tmp
+            if main_dict or temp_dict:
+                choice = main_dict[answer]
+
+        except (ValueError, KeyError) as err:
             print('\n!!! Invalid selection...\n')
+            # print(err)
         else:
             print()
             return choice
@@ -48,37 +59,71 @@ def ask(question, dict_val):
 print(line)
 
 if args.verbose:
-    print(f'Executing {__file__}...\n')
-    # print('Gathering missing information...\n')
+    print(f'Executing {__file__}...')
+    print('Loading configuration...')
 
-# ms_non_ms = ask('Connect to:\n-----------\n1) MS host\n2) Non-MS host\n\nYour choice: ')
+# ms_non_ms = prompt('Connect to:\n-----------\n1) MS host\n2) Non-MS host\n\nYour choice: ')
 
-if not args.gateway:
-    args.gateway = ask('Gateway\n-------\n1) gate-sn.ohctr.gxs.com\n2) gate-sn.avctr.gxs.com\n\nYour choice: ', gateway)
-if not args.instance:
-    args.instance = ask('Instance:\n---------\n1) instance1\n2) instance2\n\nYour choice: ', instance)
+# =====================================================================================================================================
+# parse the csv (MS_client_accounts.csv) for client environment information
+filename = 'MS_client_accounts.csv'
+csv_file = open(filename, newline='')
+reader = csv.reader(csv_file)
+
+# sample dictionary for storing client environment information
+# use instance as key when searching, value is a list of hostname, password and clientid, respectively
+# {'buaour1' : ['lit-vams-b102.gxsonline.net', '7Ey4s|Ms', 'UAOUR'], 'bcarmx1' : ['lit-vams-b102.gxsonline.net', '59K>oSgs', 'CARMX']}
+
+# parse both header and the rest of the rows
+header_list = next(reader)
+client_accounts = { row[0] : [row[1], row[2], row[3]] for row in reader}
+
+# =====================================================================================================================================
+# parse the csv (gateway_hosts.csv) for UNIX gateway information
+filename = 'gateway_hosts.csv'
+csv_file = open(filename, newline='')
+reader = csv.reader(csv_file)
+
+header_list = next(reader)
+
+# need separate dictionaries for the user prompt (if --gateway is not invoked as a parameter) and the main gateway dictionary
+gateways_tmp = {}
+gateway_hosts = {}
+counter = 1
+for row in reader:
+    gateways_tmp[counter] = row[0].lower()
+    counter += 1
+    gateway_hosts[row[0].lower()] = row[1]
+
+instance = {1 : 'baldis1', 2 : 'baldis2'}
+action = {1 : 'download', 2 : 'upload'}
+
+# determine parameters missing
+# parser.add_argument('-b', '--binary', help='if not specified, file will be treated as ASCII', action='store_true')
+# parser.add_argument('-g', '--gateway', help='UNIX gateway to be used')
+# parser.add_argument('-u', '--username', help='Gateway username')
+# parser.add_argument('-p', '--passcode', help='MobilePASS OHGate - IDLDAP.NET passcode')
+# parser.add_argument('-i', '--instance', help='MS client instance')
+# parser.add_argument('-a', '--action', help='download or upload')
+
+
+# if no --gateway argument passed, or if not recognized
+if args.gateway.lower() in gateway_hosts.keys():
+    args.gateway = gateway_hosts[args.gateway.lower()]
+elif args.gateway.lower() not in gateway_hosts.values():
+    args.gateway = prompt(header='Gateway\n-------\n', ask='\nYour choice: ', response_type='int', main_dict=gateway_hosts, temp_dict=gateways_tmp)
+
+if not args.username:
+    args.username = prompt(header='Username\n--------\n', ask='Enter username: ')
+# if not args.instance:
+#     args.instance = prompt('Instance:\n---------\n1) instance1\n2) instance2\n\nYour choice: ', instance)
 if not args.action:
-    args.action = ask('Action:\n-------\n1) download\n2) upload\n\nYour choice: ', action)
+    args.action = prompt('Action:\n-------\n', ask='\nYour choice: ', main_dict=action, response_type='int')
 
 print(f"""Gateway: {args.gateway}
 Server: {args.instance}
 """)
 
-# SSH
-"""
-key = paramiko.RSAKey(data=base64.b64decode(b'AAAAB3NzaC1kc3MAAACBAIWAZOeAHLpKxfdjeJ6VYeJRLPODV23aBJfAiOb7vAq39FElq4IJCQX80Ba5aVA4cAy9FOwwd+btnSj2ZOOazYtjI6RmAXN7ITRCuo+w+3GYlYH4JMVGDz0MGz5NbTgENWyf6Wygba5kFoQItYvc/uxsf5ekj9azeO0LW0okq/+bAAAAFQDMwX9xGV3Dr0y7+Tb0+A0UJwpZIwAAAIBt0dO7whKYI7ayY84gQ1/SN6pfZ++f3qiOT9G6o0goEPQ/UubDEAq2ucip0qumJjN5R+IEgJ02w4xrA8pPuwRjuI+iQcu+QClSE+3rxE02dvpjJr3cmmg7HfIHb91dfJoZaLPYS5YafcPd8nvHa8u68PcY524nrupem/pn1CQNTwAAAIA6eib1GWl7lAzKNpb8q633kCxJy2Ep+e+w+Jo8CCBGWtqJYTk/zS4zrf3LsxOiv+SzlsQWSeiAb8ShjxcpWpNvrHjnBhzJRFQVB+QngetO+0PU6PudGVdT+42G0zHUhUZRLXHAejaa2vEhfkITpaH2cN4OLXAC3spajdStzMLMmg=='))
-client = paramiko.SSHClient()
-client.get_host_keys().add('gate-sn.ohctr.gxs.com', 'ssh-rsa', key)
-host = 'gate-sn.ohctr.gxs.com'
-username = 'atraspe'
-port = 22
-password = '95099509819389'
-client.connect(host, port, username, password)
-stdin, stdout, stderr = client.exec_command('ls')
-for line in stdout:
-    print('... ' + line.strip('\n'))
-client.close()
-"""
 
 # FTP
 ftp_host = 'lit-vabld-q023.gxsonline.net'
@@ -98,7 +143,10 @@ with ftplib.FTP(host=ftp_host) as ftp:
         ftp.getwelcome()
         if args.verbose:
             print('Logged in...')
-        # ftp.sendcmd()
+        if args.binary:
+            ftp.sendcmd('TYPE I')
+        else:
+            ftp.sendcmd('TYPE A')
         # ftp.retrlines('LIST')
         if args.verbose:
             print(f'Starting {args.action} of {args.file}')
